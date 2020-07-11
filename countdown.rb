999ae94a80f5b9e828993beafb6889f62ef4250d
literal-bookmark-0123 <ben.stephanus@gmail.com>
2020-07-11 15:55:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -15,8 +15,8 @@ def countdown_with_sleep(number)
   while number > 0   
   puts "#{number} SECOND(S)!"
   number -=1 
-  sleep >= 1.second 
+  sleep(1.second)
   end
   "HAPPY NEW YEAR!"
 end
-countdown(1)
+countdown(5)
