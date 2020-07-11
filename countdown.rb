4fc731bbb2dca459ade1c0825a1327a0b9d39261
literal-bookmark-0123 <ben.stephanus@gmail.com>
2020-07-11 15:47:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,3 +10,13 @@ def countdown(number)
 end
 countdown(12)
 
+def countdown_with_sleep(number)
+  number = 10
+  while number > 0   
+  puts "#{number} SECOND(S)!"
+  number -=1 
+  
+  end
+  "HAPPY NEW YEAR!"
+end
+countdown(12)
